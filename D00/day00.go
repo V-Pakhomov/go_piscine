60032167df025c844b1280560cd649ca4e05869d
V-Pakhomov <pakhomov.vasiliy.v@gmail.com>
2020-09-04 12:46:41 +0300

    split main into functions

--- D00/day00.go
+++ D00/day00.go
@@ -9,44 +9,80 @@ import (
 	"math"
 )
 
-func main() {
-	var sum int = 0
-	var median float32
-	var mean float32
-	var mode int
-	var sd float64 = 0
+func arrToMap(arr []int) map[int]int {
+	dict := make(map[int]int)
+	for _, v := range arr {
+		dict[v]++
+	}
+	return dict
+}
 
-	arr := make(map[int]int)
-	slice := make([]int, 0)
+func arrSum(arr []int) int {
+	sum := 0
+	for _, v := range arr {
+		sum += v
+	}
+	return sum
+}
+
+func readStdin() []int {
+	arr := make([]int, 0)
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		num, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			if len(scanner.Text()) != 0 {
-				fmt.Println("ERROR")
+				fmt.Println("It is not an integer")
 				os.Exit(2)
 			}
 		} else {
-			slice = append(slice, num)
-			arr[num]++
-			sum += num
+			arr = append(arr, num)
 		}
 	}
-	sort.Ints(slice)
-	if len(slice) % 2 == 1 {
-		median = float32(slice[len(slice)/2])
+	sort.Ints(arr)
+	return arr
+}
+
+func calcMean(arr []int) float32 {
+	return float32(arrSum(arr)) / float32(len(arr))
+}
+
+func calcMedian(arr []int) float32 {
+	if len(arr) % 2 == 0 {
+		return float32(arr[len(arr)/2] + arr[len(arr)/2 - 1]) / 2
 	} else {
-		median = float32(slice[len(slice)/2 - 1] + slice[len(slice)/2]) / 2
+		return float32(arr[len(arr)/2])
 	}
-	mean = float32(sum) / float32(len(arr))
-	max_count := 0
-	for _, n := range slice {
-		sd += math.Pow(float64(float32(n) - mean), 2)
-		if arr[n] > max_count {
-			mode = n
-			max_count = arr[n]
+}
+
+func calcMode(arr []int) int {
+	mode := 0
+	max_amount := 0
+	dict := arrToMap(arr)
+	for _, v := range arr {
+		if dict[v] > max_amount {
+			max_amount = dict[v]
+			mode = v
 		}
+	}
+	return mode
+}
+
+func calcSD(arr []int) float64 {
+	sd := 0.0
+	mean := calcMean(arr)
+	for _, v := range arr {
+		sd += math.Pow(float64(float32(v) - mean), 2)
 	}
 	sd = math.Sqrt(sd)
+	return sd
+}
+
+func main() {
+	arr := readStdin()
+	mean := calcMean(arr)
+	median := calcMedian(arr)
+	mode := calcMode(arr)
+	sd := calcSD(arr)
 	fmt.Printf("mean = %.2f\nmedian = %.2f\nmode = %d\nSD = %.2f\n", mean, median, mode, sd)
 }
